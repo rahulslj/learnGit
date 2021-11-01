56b4c859377001e7448acfff835afd79d596bf4e
Rahul Kumar <rahulsljmail@gmail.com>
2021-11-01 12:35:08 +0530

    new Change in multiply

--- multiply.java
+++ multiply.java
@@ -2,6 +2,6 @@ public class multiply {
     public static void main(String[] args) {
         int a = 5; 
         int b = 6;
-        System.out.println("The multiply is " + (a*b));
+        System.out.println("multiply is " + (a*b));
     }
 }
